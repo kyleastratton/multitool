274e0d0e3e7f87610aea504b2b4221c7df5405a9
Kyle Stratton <76739334+kyleastratton@users.noreply.github.com>
2025-03-03 11:49:45 +0000

    Add files via upload
    Add fake personal data generator

--- script.js
+++ script.js
@@ -128,6 +128,26 @@ function showSnackbar() {
   }, 3000);
 }
 
+function generateAlphabeticalString(length) {
+  var result = "";
+  var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
+function generateNumber(length) {
+  var result = "";
+  var characters = "0123456789";
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
 function generateString() {
   const charCount = parseInt(document.getElementById("charCount").value) || 0;
   const numCount = parseInt(document.getElementById("numCount").value) || 0;
@@ -238,4 +258,777 @@ function generateStrings() {
   } catch (error) {
     outputDiv.textContent = "Error: Invalid regex or unsupported pattern.";
   }
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+// Name logic
+
+function generateName(selectorId) {
+  let maleNames = [
+    "Andrew",
+    "Adam",
+    "Adrian",
+    "Alan",
+    "Allan",
+    "Aaron",
+    "Alex",
+    "Alexander",
+    "Anthony",
+    "Austin",
+    "Archer",
+    "Arthur",
+    "Aiden",
+    "Abraham",
+    "Alfred",
+    "Alastair",
+    "Alasdair",
+    "Arnold",
+    "Angus",
+    "Ben",
+    "Benjamin",
+    "Barry",
+    "Brandon",
+    "Blair",
+    "Bryce",
+    "Blake",
+    "Brian",
+    "Bryan",
+    "Bradley",
+    "Bruce",
+    "Billy",
+    "Bob",
+    "Charles",
+    "Charlie",
+    "Carl",
+    "Connor",
+    "Cory",
+    "Calvin",
+    "Cameron",
+    "Christian",
+    "Callum",
+    "Clark",
+    "Chandler",
+    "Christopher",
+    "Chris",
+    "Callan",
+    "Craig",
+    "Daniel",
+    "David",
+    "Derek",
+    "Donald",
+    "Declan",
+    "Damian",
+    "Drew",
+    "Danny",
+    "Darren",
+    "Douglas",
+    "Duncan",
+    "Dustin",
+    "Dennis",
+    "Darnell",
+    "Desmond",
+    "Domenic",
+    "Dwight",
+    "Drake",
+    "Eric",
+    "Ethan",
+    "Elijah",
+    "Ezekiel",
+    "Elliot",
+    "Evan",
+    "Eugene",
+    "Edmund",
+    "Ewan",
+    "Earl",
+    "Emmit",
+    "Erwin",
+    "Edward",
+    "Fraser",
+    "Franic",
+    "Fred",
+    "Frank",
+    "Fletcher",
+    "Graham",
+    "Gerald",
+    "Gary",
+    "Gabriel",
+    "Gavin",
+    "George",
+    "Grant",
+    "Garrett",
+    "Gregory",
+    "Gordon",
+    "Geoffrey",
+    "Harry",
+    "Henry",
+    "Hector",
+    "Harvey",
+    "Hugo",
+    "Hank",
+    "Isaac",
+    "Irwin",
+    "James",
+    "Jamie",
+    "Jerry",
+    "John",
+    "Jack",
+    "Jacob",
+    "Joseph",
+    "Joshua",
+    "Jordon",
+    "Justin",
+    "Jeremy",
+    "Johnny",
+    "Jake",
+    "Kyle",
+    "Kevin",
+    "Kaiden",
+    "Kane",
+    "Kenneth",
+    "Kieran",
+    "Kian",
+    "Lewis",
+    "Liam",
+    "Lucas",
+    "Luke",
+    "Lachlan",
+    "Martin",
+    "Michael",
+    "Matthew",
+    "Macolm",
+    "Martin",
+    "Nathan",
+    "Nicholas",
+    "Neil",
+    "Oscar",
+    "Oliver",
+    "Owen",
+    "Peter",
+    "Paul",
+    "Patrick",
+    "Phillip",
+    "Pierce",
+    "Ross",
+    "Reese",
+    "Ryan",
+    "Rory",
+    "Richard",
+    "Steven",
+    "Stephen",
+    "Sam",
+    "Samual",
+    "Spencer",
+    "Shane",
+    "Shawn",
+    "Sean",
+    "Scott",
+    "Simon",
+    "Trevor",
+    "Thomas",
+    "Taylor",
+    "Tyler",
+    "Timothy",
+    "Troy",
+    "Tony",
+    "Toby",
+    "Todd",
+    "Victor",
+    "Vincent",
+    "William",
+    "Warren",
+    "Walter",
+    "Wayne",
+    "Zachary",
+    "Zane",
+    "Zack"
+  ];
+
+  let femaleNames = [
+    "Ashley",
+    "Amber",
+    "Alice",
+    "Alicia",
+    "Alison",
+    "Allison",
+    "Amelia",
+    "Amy",
+    "Aria",
+    "Abigail",
+    "Addison",
+    "Audrey",
+    "Anne",
+    "Anna",
+    "Alexis",
+    "Andrea",
+    "Ariel",
+    "Arianna",
+    "Alexandra",
+    "Annie",
+    "Angela",
+    "Arielle",
+    "Astrid",
+    "April",
+    "Allana",
+    "Abby",
+    "Angelica",
+    "Adrianna",
+    "Andi",
+    "Brooke",
+    "Barbara",
+    "Beverely",
+    "Bailey",
+    "Blake",
+    "Bella",
+    "Brianna",
+    "Bonnie",
+    "Bethany",
+    "Bridget",
+    "Charlotte",
+    "Chloe",
+    "Cora",
+    "Caroline",
+    "Caitlin",
+    "Carly",
+    "Clara",
+    "Catherine",
+    "Christina",
+    "Chelsea",
+    "Carolyn",
+    "Connie",
+    "Cheryl",
+    "Candice",
+    "Daisey",
+    "Danielle",
+    "Demi",
+    "Deborah",
+    "Emma",
+    "Ellie",
+    "Erica",
+    "Emily",
+    "Elizabeth",
+    "Eva",
+    "Erin",
+    "Evelyn",
+    "Elaine",
+    "Eileen",
+    "Franchesca",
+    "Florance",
+    "Gabriella",
+    "Grace",
+    "Gemma",
+    "Gracie",
+    "Gwen",
+    "Gloria",
+    "Gloria",
+    "Georgia",
+    "Hazel",
+    "Harper",
+    "Hailey",
+    "Hazel",
+    "Heidi",
+    "Holly",
+    "Helen",
+    "Hannah",
+    "Harriet",
+    "Helena",
+    "Hayley",
+    "Harrietta",
+    "Isobel",
+    "Ivy",
+    "Irene",
+    "Imogene",
+    "Jessica",
+    "Jade",
+    "Jesse",
+    "June",
+    "Juliette",
+    "Jasmine",
+    "Juliana",
+    "Jane",
+    "Juliet",
+    "Jocelyn",
+    "Jacqueline",
+    "Jennifer",
+    "Joy",
+    "Jamie",
+    "Jemma",
+    "Katie",
+    "Karen",
+    "Kylie",
+    "Kimberley",
+    "Khloe",
+    "Kiara",
+    "Kate",
+    "Keira",
+    "Kayleigh",
+    "Kelsey",
+    "Kathryn",
+    "Kathleen",
+    "Kelly",
+    "Lucy",
+    "Lauren",
+    "Lily",
+    "Leah",
+    "Lillian",
+    "Luciana",
+    "Lucille",
+    "Laura",
+    "Lexi",
+    "Leslie",
+    "Lara",
+    "Linda",
+    "Lorraine",
+    "Lesly",
+    "Lillith",
+    "Mary",
+    "Margaret",
+    "Mia",
+    "Maddison",
+    "Melanie",
+    "Madeline",
+    "Molly",
+    "Mabel",
+    "Melissa",
+    "Michelle",
+    "Murphy",
+    "Miranda",
+    "Marie",
+    "Megan",
+    "Nancy",
+    "Natalie",
+    "Natalia",
+    "Nicole",
+    "Nicola",
+    "Nadia",
+    "Natasha",
+    "Nadine",
+    "Niomi",
+    "Norma",
+    "Olivia",
+    "Ophelia",
+    "Penelope",
+    "Piper",
+    "Paige",
+    "Poppy",
+    "Pauline",
+    "Paula",
+    "Patricia",
+    "Riley",
+    "Ruby",
+    "Ruth",
+    "Ryleigh",
+    "Rachel",
+    "Rachael",
+    "Rosemary",
+    "Rosie",
+    "Rose",
+    "Robin",
+    "Robyn",
+    "Rebecca",
+    "Rhianna",
+    "Roslyn",
+    "Sophie",
+    "Sophia",
+    "Scarlett",
+    "Sarah",
+    "Sienna",
+    "Sara",
+    "Summer",
+    "Samantha",
+    "Skye",
+    "Sage",
+    "Sabrina",
+    "Sylvia",
+    "Stephanie",
+    "Sasha",
+    "Sam",
+    "Samira",
+    "Sharon",
+    "Shannon",
+    "Stacey",
+    "Susanna",
+    "Susan",
+    "Stacy",
+    "Sally",
+    "Shirley",
+    "Sheila",
+    "Shae",
+    "Siobhan",
+    "Tyler",
+    "Teagan",
+    "Teresa",
+    "Theresa",
+    "Tara",
+    "Tamara",
+    "Toni",
+    "Tahlia",
+    "Violet",
+    "Victoria",
+    "Valarie",
+    "Vanessa",
+    "Veronica",
+    "Vivian",
+    "Wendy",
+    "Whittney",
+    "Yasmine",
+    "Yvonne",
+    "Zoe",
+    "Zoey",
+    "Zara",
+    "Zahra"
+  ];
+
+  let surname = [
+    "Abbett",
+    "Abbott",
+    "Abrahams",
+    "Accord",
+    "Ackerman",
+    "Adams",
+    "Addington",
+    "Ahmed",
+    "Alderson",
+    "Allen",
+    "Altman",
+    "Anderson",
+    "Archer",
+    "Armstrong",
+    "Ash",
+    "Ashford",
+    "Ashton",
+    "Aspen",
+    "Sutherland",
+    "Sutton",
+    "Swan",
+    "Sweeney",
+    "Sweeny",
+    "Swift",
+    "Sycamore",
+    "Tait",
+    "Tanaka",
+    "Tate",
+    "Tayler",
+    "Taylor",
+    "Thomas",
+    "Thompson",
+    "Thomson",
+    "Thorne",
+    "Thornton",
+    "Thorpe",
+    "Todd",
+    "Tomlinson",
+    "Torres",
+    "Townsend",
+    "Trenton",
+    "Tucker",
+    "Turnbull",
+    "Turner",
+    "Urban",
+    "Vale",
+    "Valentia",
+    "Vaughn",
+    "Vickers",
+    "Vickers",
+    "Vincent",
+    "Waddell",
+    "Wade",
+    "Walford",
+    "Walker",
+    "Wall",
+    "Wallace",
+    "Walls",
+    "Walnut",
+    "Walsh",
+    "Walton",
+    "Ward",
+    "Warner",
+    "Waters",
+    "Watkins",
+    "Watson",
+    "Watts",
+    "Weaver",
+    "Webb",
+    "Webber",
+    "Webster",
+    "Welch",
+    "Welche",
+    "Wellick",
+    "Wells",
+    "Wernstrom",
+    "Wessing",
+    "West",
+    "Weston",
+    "Wexler",
+    "Wheaton",
+    "Wheeler",
+    "Whitaker",
+    "White",
+    "Whitebeam",
+    "Whitehead",
+    "Whitehouse",
+    "Whitman",
+    "Whitmore",
+    "Whittle",
+    "Whyte",
+    "Wilde",
+    "Wilkins",
+    "Wilkinson",
+    "Williams",
+    "Williamson",
+    "Willow",
+    "Wilson",
+    "Winter",
+    "Witcher",
+    "Wong",
+    "Wood",
+    "Woodcock",
+    "Woods",
+    "Woodward",
+    "Wright",
+    "Wyatt",
+    "Wynn",
+    "Yates",
+    "Young",
+    "Zane",
+    "Ziegler"
+  ];
+
+  const firstName = maleNames.concat(femaleNames);
+  let getFirstName = Math.floor(Math.random() * firstName.length);
+  let generatedFirstName = firstName[getFirstName];
+  let getSurname = Math.floor(Math.random() * surname.length);
+  let generatedSurname = surname[getSurname];
+  document.getElementById(selectorId).innerText =
+    generatedFirstName + " " + generatedSurname;
+}
+
+// Matter number logic
+
+function generateMatterNumber(selectorId, nameId) {
+  let name = document.getElementById(nameId).innerText;
+  let splitName = name.split(" ");
+  let surname = splitName[1];
+  let clientNumber = getRandomInt(99);
+  if (clientNumber < 10) {
+    clientNumber = clientNumber.toString().padStart(2, "0");
+  }
+  let fileNumber = getRandomInt(99);
+  if (fileNumber < 10) {
+    fileNumber = fileNumber.toString().padStart(2, "0");
+  }
+  let result = surname.substring(0, 3);
+  let result1 = result.toUpperCase();
+  let result2 = result1 + clientNumber + "-" + fileNumber;
+  document.getElementById(selectorId).innerText = result2;
+}
+
+// Address logic
+
+function getStreetPrefix(streetPrefix) {
+  const streetPrefix1 = streetPrefix;
+  return Math.floor(Math.random() * streetPrefix1.length);
+}
+
+function getStreetName(streetNames) {
+  const streetNames1 = streetNames;
+  return Math.floor(Math.random() * streetNames1.length);
+}
+
+function getTown(towns) {
+  const towns1 = towns;
+  return Math.floor(Math.random() * towns1.length);
+}
+
+function generateAddress(selectorId) {
+  const uk_cities = [
+    "Bath",
+    "Birmingham",
+    "Bradford",
+    "Brighton & Hove",
+    "Bristol",
+    "Cambridge",
+    "Canterbury",
+    "Carlisle",
+    "Chelmsford",
+    "Chester",
+    "Chichester",
+    "Colchester",
+    "Coventry",
+    "Derby",
+    "Doncaster",
+    "Durham",
+    "Ely",
+    "Exeter",
+    "Gloucester",
+    "Hereford",
+    "Kingston-upon-Hull",
+    "Lancaster",
+    "Leeds",
+    "Leicester",
+    "Lichfield",
+    "Lincoln",
+    "Liverpool",
+    "London",
+    "Manchester",
+    "Milton Keynes",
+    "Newcastle-upon-Tyne",
+    "Norwich",
+    "Nottingham",
+    "Oxford",
+    "Peterborough",
+    "Plymouth",
+    "Portsmouth",
+    "Preston",
+    "Ripon",
+    "Salford",
+    "Salisbury",
+    "Sheffield",
+    "Southampton",
+    "Southend-on-Sea",
+    "St Albans",
+    "Stoke on Trent",
+    "Sunderland",
+    "Truro",
+    "Wakefield",
+    "Wells",
+    "Westminster",
+    "Winchester",
+    "Wolverhampton",
+    "Worcester",
+    "York",
+    "Armagh",
+    "Bangor",
+    "Belfast",
+    "Lisburn",
+    "Londonderry",
+    "Newry",
+    "Aberdeen",
+    "Dundee",
+    "Dunfermline",
+    "Edinburgh",
+    "Glasgow",
+    "Inverness",
+    "Perth",
+    "Stirling",
+    "Bangor",
+    "Cardiff",
+    "Newport",
+    "St Asaph",
+    "St Davids",
+    "Swansea",
+    "Wrexham"
+  ];
+
+  const streetNames = [
+    "Road",
+    "Lane",
+    "Avenue",
+    "Boulevard",
+    "Street",
+    "Grove",
+    "Crescent"
+  ];
+
+  const streetPrefix = [
+    "Oak",
+    "Birch",
+    "Willow",
+    "Beech",
+    "Spruce",
+    "Walnut",
+    "Cherry",
+    "Maple",
+    "Alder",
+    "Ash",
+    "Aspen",
+    "Cedar",
+    "Elm",
+    "Hawthorn",
+    "Hemlock",
+    "Hornbeam",
+    "Juniper",
+    "Lime",
+    "Pine",
+    "Sycamore",
+    "Whitebeam",
+    "Yew",
+    "High",
+    "Church",
+    "Park",
+    "Main",
+    "Queens",
+    "Kings",
+    "Grange",
+    "New",
+    "Mill",
+    "North",
+    "East",
+    "South",
+    "West"
+  ];
+  const generatedNumber = getRandomInt(1000);
+  const generatedStreetPrefix = getStreetPrefix(streetPrefix);
+  const generatedStreetName = getStreetName(streetNames);
+  const generatedCity = getTown(uk_cities);
+  const generatedPostcode1 = generateAlphabeticalString(2);
+  const generatedPostcode2 = generateNumber(2);
+  const generatedPostcode3 = generateNumber(1);
+  const generatedPostcode4 = generateAlphabeticalString(2);
+  const fullPostcode =
+    generatedPostcode1 +
+    generatedPostcode2 +
+    " " +
+    generatedPostcode3 +
+    generatedPostcode4;
+
+  const address =
+    generatedNumber +
+    " " +
+    streetPrefix[generatedStreetPrefix] +
+    " " +
+    streetNames[generatedStreetName] +
+    ", " +
+    uk_cities[generatedCity] +
+    ", " +
+    fullPostcode;
+
+  document.getElementById(selectorId).innerText = address;
+}
+
+// Telephone number logic
+
+function generateTelephoneNumber(selectorId) {
+  const telephoneNumber = "01632 960 " + generateNumber(3);
+  document.getElementById(selectorId).innerText = telephoneNumber;
+}
+
+// Mobile number logic
+
+function generateMobileNumber(selectorId) {
+  const mobileNumber = "0770 090 0" + generateNumber(3);
+  document.getElementById(selectorId).innerText = mobileNumber;
+}
+
+// Sort code logic
+
+function formatSortCode(str) {
+  return (String(str).match(/.{1,2}/g) || []).join("-");
+}
+
+function generateSortCode(selectorId) {
+  let sortcode2 = generateNumber(6);
+  let sortcode3 = formatSortCode(sortcode2);
+  document.getElementById(selectorId).innerText = sortcode3;
+}
+
+// Account number logic
+
+function generateAccountNumber(selectorId) {
+  const accountnumber1 = generateNumber(4);
+  const accountnumber2 = generateNumber(4);
+  document.getElementById(selectorId).innerText =
+    accountnumber1 + " " + accountnumber2;
 }
